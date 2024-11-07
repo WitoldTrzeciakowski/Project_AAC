6bf02bfbe3001753dca483a55c27f7435a4f5330
Aleksandra Wieczorek <aleksandrawieczorek@Aleksandras-MacBook-Air.local>
2024-11-07 17:44:48 +0100

    chromatic_index() also for multigraph

--- graph_size.py
+++ graph_size.py
@@ -9,10 +9,15 @@ def count_edges_vertices(graph):
 
 def chromatic_index(graph):
     # By Vizing’s Theorem 
-    # TODO:: read https://core.ac.uk/download/pdf/82035825.pdf
+    # and generalization of this for mulit but i am not sure about this
+    # https://web.pdx.edu/~caughman/501%20Dunaway.pdf -> mulitgraph
     adjacency_matrix = graph["adjacency_matrix"]
+    max_element = max(max(sublist) for sublist in adjacency_matrix)
     v_deg = [sum(deg) for deg in adjacency_matrix]
-    return max(v_deg) , max(v_deg) + 1
+    if max_element == 1:
+        return max(v_deg) , max(v_deg) + 1
+    else: 
+        return max(v_deg) + max_element, -1
 
 file_path = 'example.txt'
 graph_data = read_file.read_graph_file(file_path)
@@ -20,8 +25,11 @@ graph_data = read_file.read_graph_file(file_path)
 for i, graph in enumerate(graph_data):
     num_edges, num_vertices = count_edges_vertices(graph)
     chromatic_index_1 , chromatic_index_2 = chromatic_index(graph)
-    print(f"Sizd of Graph {i + 1}:")
+    print(f"Size of Graph {i + 1}:")
     print("Number of vertices:", num_vertices)
     print("Number of edges:", num_edges)
-    print(f"Chromatic index is {chromatic_index_1} or {chromatic_index_2} \nFrom what i know checking which one is correct is NP-Complete <3")
+    if chromatic_index_2 == -1:
+        print(f"Chromatic index is at most {chromatic_index_1} and it is a multigraph")
+    else: 
+        print(f"Chromatic index is at most {chromatic_index_1} or {chromatic_index_2} \nFrom what i know checking which one is correct is NP-Complete <3")
     print()
