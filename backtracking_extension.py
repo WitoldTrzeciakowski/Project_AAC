b1a20b7b2a06f720c53ae6d45e85b4bf70acc700
Witold Trzeciakowski <w.trzeci@gmail.com>
2024-11-14 22:41:19 +0100

    first draft backtrack

---
+++ backtracking_extension.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+def is_hamiltonian_cycle(graph):
+    n = len(graph)
+    visited = [False] * n
+    path = []
+
+    # Start backtracking from vertex 0
+    def backtrack(v, path_length):
+        visited[v] = True
+        path.append(v)
+
+        if path_length == n:
+            # Check if there is an edge back to the starting vertex
+            if graph[v][path[0]] == 1:
+                return True
+            visited[v] = False
+            path.pop()
+            return False
+
+        for u in range(n):
+            if graph[v][u] == 1 and not visited[u]:
+                if backtrack(u, path_length + 1):
+                    return True
+
+        # Backtrack
+        visited[v] = False
+        path.pop()
+        return False
+
+    return backtrack(0, 1)
+
+def add_edge_and_check(graph, u, v):
+    original_uv = graph[u][v]
+    original_vu = graph[v][u]
+
+    # Add edge (u, v) temporarily
+    graph[u][v] = 1
+    graph[v][u] = 1
+    has_cycle = is_hamiltonian_cycle(graph)
+    
+    # Remove the edge to backtrack
+    graph[u][v] = original_uv
+    graph[v][u] = original_vu
+    return has_cycle
+
+def backtrack_to_add_edges(graph, max_edges_to_add, current_edges_added):
+    if current_edges_added > max_edges_to_add:
+        return False
+    
+    if is_hamiltonian_cycle(graph):
+        return True
+
+    n = len(graph)
+    for u in range(n):
+        for v in range(u + 1, n):
+            if graph[u][v] == 0:
+                # Add edge and recursively check
+                graph[u][v] = 1
+                graph[v][u] = 1
+                if backtrack_to_add_edges(graph, max_edges_to_add, current_edges_added + 1):
+                    return True
+                # Backtrack: remove the edge
+                graph[u][v] = 0
+                graph[v][u] = 0
+    
+    return False
+
+def find_minimum_edges_to_hamiltonian(graph):
+    max_edges_to_add = 1  # Start with trying to add just one edge
+    while True:
+        if backtrack_to_add_edges(graph, max_edges_to_add, 0):
+            return max_edges_to_add  # Minimum edges found to make graph Hamiltonian
+        max_edges_to_add += 1  # Increase edge addition limit if no solution found
+
+
+graph = np.array([
+    [0, 1, 0, 0],
+    [1, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+])
+
+print("Minimum edges to add to make the graph Hamiltonian:", find_minimum_edges_to_hamiltonian(graph))
