38b9bc14e7b4223505d0e3b0937b5fd8344a8f9d
Witold Trzeciakowski <w.trzeci@gmail.com>
2024-12-04 17:20:00 +0100

    fix added Simon methods to hamilton

--- interface_hamilton_cycle.py
+++ interface_hamilton_cycle.py
@@ -8,6 +8,7 @@ import hamilton_paths.heuristic_number_hamiltons
 import hamilton_paths.list_hamiltons
 import UnionFind
 import copy
+import maximum_cycle
 
 def do_check_on_graph(graph, isDirected):
     def print_graph_update(original, updated, label):
@@ -38,7 +39,7 @@ def do_check_on_graph(graph, isDirected):
         updated_matrix = UnionFind.find_minimal_extension_to_hamiltonian_cycle(graph_copy)
         print_graph_update(graph, updated_matrix, "DFS Approach")
         print("Exact solution to the number of cycles problem")
-        print(len(hamilton_paths.list_hamiltons.find_all_longest_cycles(updated_matrix)))
+        print(len(maximum_cycle.maximum_cycle_dfs_optimized(updated_matrix)))
         print("Approximate solution:")
         print(hamilton_paths.heuristic_number_hamiltons.approximate_hamiltonian_cycles(updated_matrix))
 
@@ -47,7 +48,7 @@ def do_check_on_graph(graph, isDirected):
         updated_matrix = Solution_proporsals.DiracTheoremSolution.add_minimal_edges_by_dirac(graph_copy, isDirected)
         print_graph_update(graph, updated_matrix, "Dirac Method")
         print("Exact solution to the number of cycles problem")
-        print(len(hamilton_paths.list_hamiltons.find_all_longest_cycles(updated_matrix)))
+        print(len(maximum_cycle.maximum_cycle_dfs_optimized(updated_matrix)))
         print("Approximate solution:")
         print(hamilton_paths.heuristic_number_hamiltons.approximate_hamiltonian_cycles(updated_matrix))
 
@@ -58,7 +59,7 @@ def do_check_on_graph(graph, isDirected):
             if updated_matrix is not None:
                 print_graph_update(graph, updated_matrix, "Spectral Theorem Solution")
                 print("Exact solution to the number of cycles problem")
-                print(len(hamilton_paths.list_hamiltons.find_all_longest_cycles(updated_matrix)))
+                print(len(maximum_cycle.maximum_cycle_dfs_optimized(updated_matrix)))
                 print("Approximate solution:")
                 print(hamilton_paths.heuristic_number_hamiltons.approximate_hamiltonian_cycles(updated_matrix))
             else:
@@ -71,7 +72,7 @@ def do_check_on_graph(graph, isDirected):
         updated_matrix = hamilton_paths.adding_edges_stack.hamiltonian_extension_matrix(graph_copy)
         print_graph_update(graph, updated_matrix, "Greedy Edge Addition Method")
         print("Exact solution to the number of cycles problem")
-        print(len(hamilton_paths.list_hamiltons.find_all_longest_cycles(updated_matrix)))
+        print(len(maximum_cycle.maximum_cycle_dfs_optimized(updated_matrix)))
         print("Approximate solution:")
         print(hamilton_paths.heuristic_number_hamiltons.approximate_hamiltonian_cycles(updated_matrix))
 
@@ -81,7 +82,7 @@ def do_check_on_graph(graph, isDirected):
         print(f"Number of edges added: {number}")
         print_graph_update(graph, updated_matrix, "Exact Solution for Minimal Edges")
         print("Exact solution to the number of cycles problem")
-        print(len(hamilton_paths.list_hamiltons.find_all_longest_cycles(updated_matrix)))
+        print(len(maximum_cycle.maximum_cycle_dfs_optimized(updated_matrix)))
         print("Approximate solution:")
         print(hamilton_paths.heuristic_number_hamiltons.approximate_hamiltonian_cycles(updated_matrix))
         print()
@@ -89,6 +90,6 @@ def do_check_on_graph(graph, isDirected):
         
     if backtrack or spectral or dirac:
         print("----- Number Of Hamilton Cycles -----")
-        print("Exact solution to the number of cycles problem", len(hamilton_paths.list_hamiltons.find_all_longest_cycles(graph)))
+        print("Exact solution to the number of cycles problem", len(maximum_cycle.maximum_cycle_dfs_optimized(graph)))
         print("Approximate solution:", hamilton_paths.heuristic_number_hamiltons.approximate_hamiltonian_cycles(graph))
         print()
